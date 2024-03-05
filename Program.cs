069d95a88740b779b5b9fc0bc1bc2677afb917be
nagihanakkus <nagihan.akkus.76@gmail.com>
2024-03-05 23:38:13 +0300

    question 5 completed

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Bir sayının mükemmel sayı olup olmadığı kontrol eden algoritma yazınız.
+
+while (true)
+{
+    Console.Write("Lütfen bir sayı giriniz : ");
+    double number = double.Parse(Console.ReadLine());
+    double total = 0;
+
+    for (double i = 1; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            total += i;
+        }
+    }
+
+    if (total == number)
+    {
+        Console.WriteLine($"{number} sayısı mükemmel sayıdır.\n");
+    }
+    else
+    {
+        Console.WriteLine($"{number} sayısı mükemmel sayı değildir.\n");
+    }
+}
+
