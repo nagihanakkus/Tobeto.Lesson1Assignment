09cb1ed27b2f40d59472d97fd1985e7864c13385
nagihanakkus <nagihan.akkus.76@gmail.com>
2024-03-05 22:37:52 +0300

    question 4 completed

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿
+// Kullanıcının 1 ile 10 arasında rastgele bir sayıyı tahmin etmesini isteyen
+// ve doğru tahmin edene kadar devam eden bir program yazınız.
+// (While döngüsü ile yapınız ve rastgele sayı üretmek için Random sınıfını kullanınız)
+
+Random random = new Random();
+int randomNumber = random.Next(1, 10);
+
+while (true)
+{
+    Console.Write("Lütfen bir sayı giriniz : ");
+    int guess = int.Parse(Console.ReadLine());
+    if (guess == randomNumber)
+    {
+        Console.WriteLine("Tebrikler Bildiniz\n");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Tekrar Deneyin\n");
+    }
+}
